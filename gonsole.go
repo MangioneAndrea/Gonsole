6c65decad8c831367649f47b09f89f0a979d3d70
Andrea Mangione <35958935+MangioneAndrea@users.noreply.github.com>
2022-01-24 21:15:20 +0100

    Revert "remove custom colors in favour of tcell"
    This reverts commit 5717df7cf721b5a72edaff0e57e82c513c1c1a5d.

--- gonsole.go
+++ gonsole.go
@@ -2,7 +2,6 @@ package gonsole
 
 import (
 	"fmt"
-	"github.com/gdamore/tcell"
 	"io"
 	"os"
 )
@@ -10,10 +9,24 @@ import (
 var out io.Writer = os.Stdout
 var in io.Writer = os.Stdin
 
+type Color string
+
+var (
+	Default = Color("")
+	Black   = Color("\033[1;30m%s\033[0m")
+	Red     = Color("\033[1;31m%s\033[0m")
+	Green   = Color("\033[1;32m%s\033[0m")
+	Yellow  = Color("\033[1;33m%s\033[0m")
+	Purple  = Color("\033[1;34m%s\033[0m")
+	Magenta = Color("\033[1;35m%s\033[0m")
+	Teal    = Color("\033[1;36m%s\033[0m")
+	White   = Color("\033[1;37m%s\033[0m")
+)
+
 // Color Format text with custom color
-func setColor(colorString tcell.Color) func(...interface{}) string {
+func setColor(colorString Color) func(...interface{}) string {
 	sprint := func(args ...interface{}) string {
-		if colorString == tcell.ColorDefault {
+		if colorString == Default {
 			return fmt.Sprint(args...)
 		}
 		return fmt.Sprintf(string(colorString),
@@ -38,12 +51,11 @@ func resolveProps(props []interface{}) (interface{}, ShowIf) {
 	return nil, ShowAlways
 }
 
-func Print(a interface{}, desc interface{}, showIf ShowIf, color tcell.Color) {
+func Print(a interface{}, desc interface{}, showIf ShowIf, color Color) {
 	if showIf(a) {
 		if desc != nil {
 			fmt.Fprint(out, setColor(color)(desc), setColor(color)(": "))
 		}
-
 		fmt.Printf("set'%s'", setColor(color)(a))
 		fmt.Fprintln(out, setColor(color)(a))
 	}
@@ -52,23 +64,23 @@ func Print(a interface{}, desc interface{}, showIf ShowIf, color tcell.Color) {
 // Log Print a text
 func Log(a interface{}, props ...interface{}) {
 	desc, showIf := resolveProps(props)
-	Print(a, desc, showIf, tcell.ColorDefault)
+	Print(a, desc, showIf, Default)
 }
 
 // Success Print a text green
 func Success(a interface{}, props ...interface{}) {
 	desc, showIf := resolveProps(props)
-	Print(a, desc, showIf, tcell.ColorGreen)
+	Print(a, desc, showIf, Green)
 }
 
 // Error Print a text red
 func Error(a interface{}, props ...interface{}) {
 	desc, showIf := resolveProps(props)
-	Print(a, desc, showIf, tcell.ColorRed)
+	Print(a, desc, showIf, Red)
 }
 
 // Warn Print a text yellow
 func Warn(a interface{}, props ...interface{}) {
 	desc, showIf := resolveProps(props)
-	Print(a, desc, showIf, tcell.ColorYellow)
+	Print(a, desc, showIf, Yellow)
 }
